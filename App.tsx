d0de9d09ed3c1882f2ccd287b8bcd36d1c1429c5
eslan94 <eslan94.eca@gmail.com>
2024-02-02 23:13:14 -0500

    Ejercicio2_3

--- App.tsx
+++ App.tsx
@@ -2,11 +2,17 @@
 import React from 'react'
 import { SafeAreaView } from 'react-native'
 import { ArregloScreen } from './src/screens/ArregloScreen'
+import { Ej1Screen } from './src/screens/Ej1Screen'
+import { Ej2Screen } from './src/screens/Ej2Screen'
+import { Ej3Screen } from './src/screens/Ej3Screen'
 
 export const App = () => {
   return (
     <SafeAreaView style={{flex:1}}>
-      <ArregloScreen/>
+      {/*<ArregloScreen/>*/}
+      {/*<Ej1Screen/>*/}
+      {/*<Ej2Screen/>*/}
+      <Ej3Screen/>
     </SafeAreaView>
   )
 }
--- src/screens/Ej3Screen.tsx
+++ src/screens/Ej3Screen.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { StyleSheet, View } from 'react-native'
 
-export const Ej1Screen = () => {
+export const Ej3Screen = () => {
   return (
     <View style={styles.container}>
         <View style={styles.boxPurple}></View>
@@ -15,31 +15,29 @@ const styles=StyleSheet.create({
     container:{
         flex:1,
         backgroundColor: '#273746',
-        alignItems:'flex-end'
+        justifyContent: 'center',
     },
-
     boxPurple:{
-        flex:1,
-        width:100,
+        width: 100,
+        height: 100,
         backgroundColor: '#4A235A',
         borderColor: 'white',
         borderWidth:7,
+        alignSelf:'flex-end'
     },
-
     boxOrange:{
-      flex:7,
       width:100,
+      height: 100,
       backgroundColor: '#E67E22',
       borderColor: 'white',
       borderWidth:7,
     },
-
     boxBlue:{
-      flex:1,
+      height:100,
       width:100,
       backgroundColor: '#48C9B0',
       borderColor: 'white',
       borderWidth:7,
-      
-    },
+      alignSelf:'center'
+    }
 })
